3b80c291eb29654af062669f76143c27d8eb7d8c
MiguelKroukamp <kroukampm@gmail.com>
2018-10-23 16:38:47 +0200

    added font and background

--- js/alert.js
+++ js/alert.js
@@ -6,14 +6,14 @@ document.getElementById("btn").onclick=function changText()
     document.getElementById("gif").style.display = "block";
     document.getElementById("btn").style.display = "none";
     document.getElementById("gif").style.margin = "auto";
-    document.getElementById("gif").style.marginTop = "32vh";
+    document.getElementById("gif").style.marginTop = "23vh";
 }
 document.getElementById("btn").style.animation = "translateY(3px)";
 document.getElementById("btn").style.fontSize ="50px";
 document.getElementById("demo").style.fontSize ="50px";
 document.getElementById("demo").style.textAlign = "center";
 document.getElementById("demo").style.fontFamily = "'Press Start 2P', cursive";
-document.getElementById("demo").style.textShadow = "2px 2px 2px black";
+document.getElementById("demo").style.textShadow = "2px 2px 2px red";
 document.getElementById("btn").style.display = "block";
 document.getElementById("btn").style.margin = "auto";
 }
