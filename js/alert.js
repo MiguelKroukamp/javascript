bcda86c0ef5f3d23d06db7e36bb5ca5cabb7cf74
MiguelKroukamp <kroukampm@gmail.com>
2018-10-23 14:05:05 +0200

    first attempt in JavaScript. added a message. added a click box

---
+++ js/alert.js
@@ -0,0 +1,19 @@
+alert("I'm learning JavaScript");
+alert("and it's LIT!!!");
+document.getElementById("btn").onclick=function changText(){ document.getElementById("demo").innerHTML ="🖕 OFF FOR YOU 🖕";
+document.getElementById("btn").onclick=function changText() 
+{
+    document.getElementById("gif").style.display = "block";
+    document.getElementById("btn").style.display = "none";
+    document.getElementById("gif").style.margin = "auto";
+    document.getElementById("gif").style.marginTop = "32vh";
+}
+document.getElementById("btn").style.animation = "translateY(3px)";
+document.getElementById("btn").style.fontSize ="50px";
+document.getElementById("demo").style.fontSize ="50px";
+document.getElementById("demo").style.textAlign = "center";
+document.getElementById("demo").style.fontFamily = "'Press Start 2P', cursive";
+document.getElementById("demo").style.textShadow = "2px 2px 2px black";
+document.getElementById("btn").style.display = "block";
+document.getElementById("btn").style.margin = "auto";
+}
---
+++ js/main.js
@@ -0,0 +1,5 @@
+// This is an alert
+
+// alert("Hi, this is Miguel")
+
+console.log("Miguel is here")
